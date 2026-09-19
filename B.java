79e4c34cfd562e96f16bebd82fc5ed63c2155319
Bruno Custódio <brunocustodio.inf@gmail.com>
2026-09-19 17:28:43 -0300

    Criação das classes A e B com seus atributos e métodos

---
+++ B.java
@@ -0,0 +1,30 @@
+public class B {
+    // ATRIBUTOS:
+    int B1;
+    float B2;
+
+    // METODOS:
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }
+
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
